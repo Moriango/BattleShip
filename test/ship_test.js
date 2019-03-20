3aaf7a4f13da93a5570120a07474569f4f402983
Toddneal Stallworth <toddnealstallworth@gmail.com>
2019-03-20 14:07:28 -0700

    First Commit

---
+++ test/ship_test.js
@@ -0,0 +1,97 @@
+var expect = require('chai').expect;
+
+describe('checkForShip', function (){
+    var checkForShip = require('../game_logic/ship_methods').checkForShip;
+    var player;
+    before(function (){
+        player = {
+            ships: [
+                {
+                    locations: [[0, 0], [0, 1]]
+                },
+                {
+                    locations: [[1, 0], [1, 1]]
+                },
+                {
+                    locations: [[2, 0], [2, 1], [2, 2], [2, 3]]
+                }
+            ]
+        };
+    });
+    it('should correctly report no ship at a given player coordinate', function (){
+        
+        expect(checkForShip(player, [9, 9])).to.be.false;
+    });
+    it('should correctly report a ship at a given player coordinates', function (){
+        
+        expect(checkForShip(player, [0, 0])).to.deep.equal(player.ships[0]);
+    });
+    it('should handle ships at more than one coordinate', function (){
+        player = {
+            ships: [
+                {
+                    locations: [[0, 0], [0, 1]]
+                }
+            ]
+        };
+        expect(checkForShip(player, [0, 1])).to.deep.equal(player.ships[0]);
+        expect(checkForShip(player, [0, 0])).to.deep.equal(player.ships[0]);
+        expect(checkForShip(player, [9, 9])).to.be.false;
+    });
+    it('should handle checking multiple ships', function (){
+        player = {
+            
+        };
+        expect(checkForShip(player, [0, 1])).to.deep.equal(player.ships[0]);
+        expect(checkForShip(player, [0, 0])).to.deep.equal(player.ships[0]);
+        expect(checkForShip(player, [1, 0])).to.deep.equal(player.ships[1]);
+        expect(checkForShip(player, [1, 1])).to.deep.equal(player.ships[1]);
+        expect(checkForShip(player, [2, 3])).to.deep.equal(player.ships[2]);
+        expect(checkForShip(player, [9, 9])).to.be.false;
+    });
+});
+
+describe('damageShip', function () {
+    var damageShip = require('../game_logic/ship_methods').damageShip;
+    var player;
+
+    beforeEach(function () {
+        player = {
+            ship: [
+                {
+                    locations: [[0, 0]],
+                    damage: []
+                }
+            ]
+        }
+    });
+
+    it('should register damage on a given ship at a given location', function () {
+        damageShip(ship, [0, 0]);
+
+        expect(ship.damage).to.not.be.empty;
+        expect(ship.damage[0]).to.deep.equal([0, 0]);
+    });
+})
+
+describe('fire', function () {
+    var fire = require('../game_logic/ship_methods').fire;
+
+    it('should record damage on a given players ship at a given coordinate', function () {
+        
+        fire(player, [0, 0]);
+        expect(player.ships[0].damage[0]).to.deep.equal([0, 0]);
+    });
+    it('should not record damage if there is no ship at my coordinates', function () {
+        var player = {
+            ships: [
+                {
+                    locations: [[9, 9]],
+                    damage: []
+                }
+            ]
+        };
+        fire(player, [0, 0]);
+        expect(player.ships[0].damage).to.be.empty;
+    });
+});
---
+++ test/test.js
@@ -0,0 +1,10 @@
+var expect = require('chai').expect;
+//Test suite 
+
+//Sanity test makes sure everything works
+describe('Mocha', function (){
+    //Test spec (unit test)
+    it('should run our tests using npm', function(){
+        expect(true).to.be.ok;
+    });
+});
